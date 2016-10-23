110bad70ebe4630d68589c8ca3ca8b77c5153e2a
Evgen Marchenko <docccdev@MacBook-Pro-Evgen.local>
2016-10-23 16:12:14 +0300

    change server ip to 127.0.0.1:3000

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, '192.168.1.7', function (err, result) {
+}).listen(3000, '127.0.0.1', function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at 192.168.1.7:3000');
+  console.log('Listening at 127.0.0.1:3000');
 });
